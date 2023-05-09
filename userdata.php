15d564959e59d71ba48100f088a4cefb918c41bf
Timosssshka <112942954+Timosssshka@users.noreply.github.com>
2023-05-09 22:36:30 +0300

    Update userdata.php

--- userdata.php
+++ userdata.php
@@ -26,7 +26,7 @@ try {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>User Data</title>
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="style2.css">
 </head>
 <body>
     <div class="container">
